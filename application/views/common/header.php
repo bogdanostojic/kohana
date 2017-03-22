5b48b76958393e9e05909d9d988e02a9797f2be1
Bogdan Ostojic <alukobond93@gmail.com>
2017-03-22 15:42:07 +0100

    male izmene

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -3,17 +3,18 @@
 </div> 
 <p id=»tagline»>   <em>Because it›s all about you!</em> </p>
 <ul id=»main_nav»> 
-    <li><a href="<?php echo URL::site(); ?>">Home</a></li> 
-    <li><a href="<?php echo URL::site("page/about"); ?>">About <?php echo $site_name; ?></a></li>  
-    <li><a href="<?php echo URL::site("page/why_egotist"); ?>">Why use Egotist?</a></li>
-      <li><a href="<?php echo URL::site("/messages/add/1"); ?>">Add message</a></li>
+
 </ul> 
 
 <p id="account">
 
 <?php if (Auth::instance()->logged_in()       && $user = Auth::instance()->get_user()) : ?>      Logged in as <?php echo $user->username; ?>.  <?php echo HTML::anchor('logout', 'Logout'); ?>
 	<?php echo HTML::anchor('messages/add', 'New Message'); ?>
+	   <li><a href="<?php echo URL::site(); ?>">Home</a></li> 
+	       <li><a href="<?php echo URL::site("page/about"); ?>">About <?php echo $site_name; ?></a></li>  
+    <li><a href="<?php echo URL::site("page/why_egotist"); ?>">Why use Egotist?</a></li>
 <?php else: ?>
+	<br>
        <?php echo HTML::anchor('login', 'Login'); ?> | <?php echo HTML::anchor('signup', 'Signup'); ?>
 <?php endif; ?>
 
